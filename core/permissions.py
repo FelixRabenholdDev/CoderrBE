452c3ed939ac9507e6e32dbaea248d75a65dd7b1
Felix R <nikojin@gmx.net>
2026-05-18 12:01:15 +0200

    add global permissions.py

---
+++ core/permissions.py
@@ -0,0 +1,37 @@
+from rest_framework.permissions import BasePermission
+from profiles_app.models import UserProfile
+
+
+class IsCustomerUser(BasePermission):
+    """
+    Allows access only to users with a customer profile.
+    """
+
+    def has_permission(self, request, view):
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            return profile.type == "customer"
+        except UserProfile.DoesNotExist:
+            return False
+
+
+class IsBusinessUser(BasePermission):
+    """
+    Allows access only to users with a business profile.
+    """
+
+    def has_permission(self, request, view):
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            return profile.type == "business"
+        except UserProfile.DoesNotExist:
+            return False
+
+
+class IsAdminUser(BasePermission):
+    """
+    Allows access only to staff/admin users.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
